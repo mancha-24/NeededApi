26e2b48135e65d1410c59a92e0a38a840a50ba33
Manuel Chavez <61197986+mancha-24@users.noreply.github.com>
2024-06-07 16:08:16 -0500

    chore: update AssemblyInfo.cs with new informational version

--- obj/Debug/net8.0/NeededApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/NeededApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeededApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba9d60ffa76c82b4fc3b22977f459610673411d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeededApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeededApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
